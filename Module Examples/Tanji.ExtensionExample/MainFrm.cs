fb1f45cf40428843865b3e686cd175ec7f007c57
ArachisH <HuitronAJ@gmail.com>
2017-05-10 19:52:06 -0700

    Added Tanji module examaple projects.

---
+++ Module Examples/Tanji.ExtensionExample/MainFrm.cs
@@ -0,0 +1,13 @@
+﻿using Tangine.Modules;
+
+namespace Tanji.ExtensionExample
+{
+    [Module("Extension Example", "Module example showing how to create a valid extension(UI) for Tanji.")]
+    public partial class MainFrm : ExtensionForm
+    {
+        public MainFrm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Module Examples/Tanji.ServiceExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tangine.Modules;
+
+namespace Tanji.ServiceExample
+{
+    [Module("Service Example", "Module example showing how to create a valid service(no-UI) for Tanji.")]
+    public class Program : TService
+    {
+        public Program(string[] args)
+        { }
+        public static void Main(string[] args)
+        {
+            var app = new Program(args);
+            app.Run();
+        }
+
+        public void Run()
+        { }
+    }
+}
