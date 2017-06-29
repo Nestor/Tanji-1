fc3a9d870a44340a50597c78d788b4f5488a389c
ArachisH <HuitronAJ@gmail.com>
2017-06-29 13:50:42 -0700

    Unfinished constructer stuff

--- Tanji/Services/Injection/Constructer/ConstructerViewModel.cs
+++ Tanji/Services/Injection/Constructer/ConstructerViewModel.cs
@@ -1,7 +1,37 @@
-﻿using Tanji.Helpers;
+﻿#define DEBUG_CONSTRUCTER
+
+using System.Collections.ObjectModel;
+
+using Tanji.Helpers;
+using Tanji.Services.Injection.Constructer.Models;
 
 namespace Tanji.Services.Injection.Constructer
 {
     public class ConstructerViewModel : ObservableObject
-    { }
+    {
+        public ObservableCollection<Chunk> Chunks { get; }
+
+        private Chunk _selectedChunk = null;
+        public Chunk SelectedChunk
+        {
+            get => _selectedChunk;
+            set
+            {
+                _selectedChunk = value;
+                RaiseOnPropertyChanged();
+            }
+        }
+
+        public ConstructerViewModel()
+        {
+            Chunks = new ObservableCollection<Chunk>();
+
+#if DEBUG_CONSTRUCTER
+            for (int i = 0; i < 10; i++)
+            {
+                Chunks.Add(new Chunk());
+            }
+#endif
+        }
+    }
 }
---
+++ Tanji/Services/Injection/Constructer/Models/Chunk.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Tanji.Helpers;
+
+namespace Tanji.Services.Injection.Constructer.Models
+{
+    public class Chunk : ObservableObject
+    {
+        private string _value = null;
+        public string Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                RaiseOnPropertyChanged();
+            }
+        }
+
+        public Chunk()
+        {
+            Value = Guid.NewGuid().ToString();
+        }
+    }
+}
